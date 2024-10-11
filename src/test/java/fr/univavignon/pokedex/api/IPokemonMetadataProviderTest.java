2985906d662952f8396b79f2e5bada320c38510c
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-10-11 12:50:32 +0200

    build(pom): add junit 5 version

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -0,0 +1,18 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IPokemonMetadataProviderTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPokemonMetadata() {
+    }
+}

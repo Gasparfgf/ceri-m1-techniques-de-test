87e8576877924139bad9fc0088aaf7c9a8ea5c31
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-10-11 18:00:25 +0200

    test(IPokemonMetadataProviderTest): test build with success

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,18 +1,35 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class IPokemonMetadataProviderTest {
+    private IPokemonMetadataProvider myInterface;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
+        myInterface = mock(IPokemonMetadataProvider.class);
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
+    @Test
+    void testGetPokemonMetadata() throws PokedexException {
+        // je fabrique les données
+        int index = 0;
+        PokemonMetadata pokemon = new PokemonMetadata(index, "Aquali", 186, 168, 260);
 
-    @org.junit.jupiter.api.Test
-    void getPokemonMetadata() {
+        // j'appele l'action
+        when(myInterface.getPokemonMetadata(index)).thenReturn(pokemon);
+
+        // je vérifie si le résultat est bien une instance de PokemonMetadata
+        assertInstanceOf(PokemonMetadata.class, pokemon);
+
+        // je garantis que les données renvoyées sont correctes
+        assertEquals("Aquali",pokemon.getName());
+        assertEquals(186, pokemon.getAttack());
+        assertEquals(168, pokemon.getDefense());
+        assertEquals(260, pokemon.getStamina());
+
     }
 }

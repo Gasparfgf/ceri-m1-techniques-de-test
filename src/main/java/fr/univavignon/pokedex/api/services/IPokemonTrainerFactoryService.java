aabe396d2c007b1a8639cefa1bf61faee7e9aadc
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-11-07 11:34:53 +0100

    feat: IPokemonTrainerFactoryService implementation

--- src/main/java/fr/univavignon/pokedex/api/services/IPokemonTrainerFactoryService.java
+++ src/main/java/fr/univavignon/pokedex/api/services/IPokemonTrainerFactoryService.java
@@ -1,4 +1,29 @@
 package fr.univavignon.pokedex.api.services;
 
-public class IPokemonTrainerFactoryService {
+import fr.univavignon.pokedex.api.models.PokemonTrainer;
+import fr.univavignon.pokedex.api.models.Team;
+import fr.univavignon.pokedex.api.repositories.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IPokemonTrainerFactoryService implements IPokemonTrainerFactory {
+    private IPokemonMetadataProvider metadataProvider;
+    private final IPokemonFactory pokemonFactory;
+    private List<PokemonTrainer> pokemonTrainerList;
+
+    public IPokemonTrainerFactoryService(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory){
+        this.pokemonTrainerList = new ArrayList<>();
+        this.metadataProvider = metadataProvider;
+        this.pokemonFactory = pokemonFactory;
+    }
+
+
+    @Override
+    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+        IPokedex pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+        PokemonTrainer trainer = new PokemonTrainer(name, team, pokedex);
+        pokemonTrainerList.add(trainer);
+        return trainer;
+    }
 }

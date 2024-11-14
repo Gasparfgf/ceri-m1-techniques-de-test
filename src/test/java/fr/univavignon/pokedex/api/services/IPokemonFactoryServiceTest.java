108324c60adb14f75e76b8964ad53d3d1586a10b
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-11-14 21:09:28 +0100

    test: IPokemonFactoryServiceTest implementation

---
+++ src/test/java/fr/univavignon/pokedex/api/services/IPokemonFactoryServiceTest.java
@@ -0,0 +1,26 @@
+package fr.univavignon.pokedex.api.services;
+
+import fr.univavignon.pokedex.api.models.Pokemon;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IPokemonFactoryServiceTest {
+    private IPokemonFactoryService factory;
+
+    @BeforeEach
+    void setUp() {
+        factory = new IPokemonFactoryService();
+    }
+
+    @Test
+    void testCreatePokemon() {
+        Pokemon pokemon = factory.createPokemon(0, 500, 100, 3000, 3);
+        assertNotNull(pokemon);
+        assertEquals("Bulbizarre", pokemon.getName());
+        assertEquals(126, pokemon.getAttack());
+        assertEquals(500, pokemon.getCp());
+    }
+
+}

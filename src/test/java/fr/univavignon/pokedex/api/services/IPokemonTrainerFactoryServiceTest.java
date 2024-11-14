77249cb2e3c2c73aa0a0a68b0e037e0e097061d6
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-11-14 21:25:14 +0100

    test: IPokemonTrainerFactoryServiceTest implementation

---
+++ src/test/java/fr/univavignon/pokedex/api/services/IPokemonTrainerFactoryServiceTest.java
@@ -0,0 +1,42 @@
+package fr.univavignon.pokedex.api.services;
+
+import fr.univavignon.pokedex.api.models.PokemonTrainer;
+import fr.univavignon.pokedex.api.models.Team;
+import fr.univavignon.pokedex.api.repositories.IPokedex;
+import fr.univavignon.pokedex.api.repositories.IPokedexFactory;
+import fr.univavignon.pokedex.api.repositories.IPokemonFactory;
+import fr.univavignon.pokedex.api.repositories.IPokemonMetadataProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class IPokemonTrainerFactoryServiceTest {
+    private IPokemonTrainerFactoryService trainerFactory;
+    private IPokemonMetadataProvider metadataProvider;
+    private IPokemonFactory pokemonFactory;
+    private IPokedexFactory pokedexFactory;
+
+    @BeforeEach
+    void setUp() {
+        metadataProvider = mock(IPokemonMetadataProvider.class);
+        pokemonFactory = mock(IPokemonFactory.class);
+        pokedexFactory = mock(IPokedexFactory.class);
+        trainerFactory = new IPokemonTrainerFactoryService(metadataProvider, pokemonFactory);
+    }
+
+    @Test
+    void testCreateTrainer() {
+        IPokedex pokedex = mock(IPokedex.class);
+        when(pokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(pokedex);
+
+        PokemonTrainer trainer = trainerFactory.createTrainer("Ash", Team.VALOR, pokedexFactory);
+        assertNotNull(trainer);
+        assertEquals("Ash", trainer.getName());
+        assertEquals(Team.VALOR, trainer.getTeam());
+        assertEquals(pokedex, trainer.getPokedex());
+    }
+
+}
